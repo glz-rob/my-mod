39be8266b05a1671b40f1a7d97f2cbc67dbb5420
rob <rcgr.13@gmail.com>
2024-10-11 13:51:47 -0700

    Added a custom item group

--- src/main/java/com/glzrob/ModItems.java
+++ src/main/java/com/glzrob/ModItems.java
@@ -1,10 +1,14 @@
 package com.glzrob;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
@@ -23,17 +27,27 @@ public class ModItems {
 
     // --------------------  END ITEM REGISTRIES  --------------------
 
+    public static final RegistryKey<ItemGroup> CUSTOM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(MyMod.MOD_ID, "item_group"));
+    public static final ItemGroup CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
+        .icon(() -> new ItemStack(ModItems.SUSPICIOUS_MINERAL))
+        .displayName(Text.translatable("itemGroup.my-mod"))
+        .build();
+
+
     /**
      * Initialize the Mod Items. Create and add items to new ItemGroup
      */
     public static void initialize() {
         // Get the event for modifying entries in the ingredients group
         // And register an event handler that adds our suspicious item to the ingredients group
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
-            .register((itemGroup) -> itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE));
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
-            .register((itemGroup) -> itemGroup.add(ModItems.SUSPICIOUS_MINERAL));
+        Registry.register(Registries.ITEM_GROUP, CUSTOM_GROUP_KEY, CUSTOM_ITEM_GROUP);
+
+        ItemGroupEvents.modifyEntriesEvent(CUSTOM_GROUP_KEY).register(itemGroup -> {
+            itemGroup.add(ModItems.SUSPICIOUS_MINERAL);
+            itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE);
+        });
+
     }
 
     /**
